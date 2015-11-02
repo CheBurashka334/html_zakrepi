bfad65ecad881c62bf4a5a512b413e1a2307c88b
myaff <grabka74@gmail.com>
2015-11-02 11:33:03 +0500

    тест-драйв

---
+++ test-drive.php
@@ -0,0 +1,96 @@
+<?php include('header.php');?>
+<div class="breadcrumbs">
+	<a class="brdcmb-link" href="#">Главная</a> / 
+	<a class="brdcmb-link" href="#">Тест-Драйв</a>
+</div>
+<div class="workarea">
+<div class="page-title">Классификация электролобзиков</div>
+<div class="action-single test-drive">
+	<div class="date color-text text-light">25 сентября 2015</div>
+	<div class="item-img"><img src="images/content/actions/action-full.jpg"/></div>
+	<div class="row">
+		<div class="col l9 center item-text">
+			<p>На рынке электрических инструментов сейчас представлено большое количество моделей электролобзиков, которые отличаются друг от друга типом электропитания, характеристиками, а также особенностями конструкции.</p>
+			<p>В зависимости от характера использования электролобзики классифицируют на промышленные, бытовые и профессиональные. Бытовые модели лобзиков стоят недорого, они имеют относительно небольшую мощность, достаточную для бытовых нужд. Профессиональные электролобзики обладают большой мощностью, высокой износостойкостью, улучшенной комплектацией и расширенными характеристиками. Промышленные модели лобзиков представляют собой станки, необходимые для работы в деревообрабатывающей промышленности.</p>
+			<p>В зависимости от типа питания электролобзики классифицируют на аккумуляторные и сетевые. Аккумуляторные модели мобильны, однако при их покупке следует первым делом обратить на тип аккумумлятора, желательно, чтобы это была литий-ионная батарея, которая отличается улучшенными эксплуатационными характеристиками. От емкости батареи в первую очередь зависит продолжительность работы электролобзика без подзарядки, а от напряжения батареи зависит производительность. </p>
+			<p>В зависимости от конструктивных особенностей электролобзики разделяют на лобзики со скобовидной и с грибовидной ручками. </p>
+		</div>
+		<div class="product-box col l9 center clearfix no-padding">
+			<div class="divider"></div>
+			<div class="box-title">В этой статье упоминался следующий товар:</div>
+			<div class="product-list row">
+				<div class="item col l3">
+					<div class="product-card">
+						<div class="product-info">
+							<a class="item-img" href="#" style="background-image:url('images/content/products/cataloge-product-1.jpg');"></a>
+							<div class="item-name"><a href="#">Лобзик электрический МП 85 600 Э "ИНТЕРСКОЛ"</a></div>
+							<div class="rating rate-2">
+								<svg class="star"><use xlink:href="#star"/></svg>
+								<svg class="star"><use xlink:href="#star"/></svg>
+								<svg class="star"><use xlink:href="#star"/></svg>
+								<svg class="star"><use xlink:href="#star"/></svg>
+								<svg class="star"><use xlink:href="#star"/></svg>
+							</div>
+							<div class="product-price">
+								<div class="price">7 299 <i class="rouble">i</i></div>
+							</div>
+							<a href="#" class="shopping-card btn btn-icon primary">
+								<svg class="icon"><use xlink:href="#cart"/>
+							</a>
+						</div>
+						<div class="compare">
+							<input type="checkbox" id="compare_today_1" />
+							<label class="checkbox-lbl" for="compare_today_1">Cравнить</label>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="comments-box col l9 center clearfix no-padding">
+			<div class="box-title">Комментарии</div>
+			<div class="comments-list col l9 center clearfix no-padding">
+				<div class="comment-item">
+					<span class="item-name big-text">Яша Ахмедчук</span>
+					<span class="item-date color-text text-light">25 сентября 2015</span>
+					<p class="item-text">При встрече с такими простыми формулировками легко может сложиться впечатление, что и сама суть предпринимательства, особенности человека-предпринимателя представлены с подобной же ясностью, и достаточно ознакомиться с ними, чтобы понять все требования, которым должен отвечать человек, желающий справиться с этой ролью.</p>
+				</div>
+				<div class="comment-item">
+					<span class="item-name big-text">Алексей Алексеев</span>
+					<span class="item-date color-text text-light">25 сентября 2015</span>
+					<p class="item-text">Вот основные, на мой взгляд, качества предпринимателя: желание идти на риск ( 'разумный' риск); готовность к любым изменениям на рынке; склонность к инновациям ( нововведениям ).</p>
+				</div>
+				<div class="comment-item">
+					<span class="item-name big-text">Пётр Семёнов</span>
+					<span class="item-date color-text text-light">25 сентября 2015</span>
+					<p class="item-text">Прежде чем принимать решение о начале предпринимательской деятельности, необходимо выполнить объективную самооценку. Творческими способностями, изобретательностью, воображением, необходимыми для решения предпринимательских (организационных, управленческих, маркетинговых) проблем, наделены многие.</p>
+				</div>
+			</div>
+			<div class="box-title">Оставьте свой комментарий</div>
+			<div class="comment-form clearfix">
+				<div class="row">
+					<div class="col l12 no-padding">
+						<div class="col l6">
+							<div class="field">
+								<label>Имя*</label>
+								<input type="text"/>
+							</div>
+						</div>
+						<div class="col l6">
+							<div class="field">
+								<label>Электронная почта*</label>
+								<input type="text"/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="field">
+					<label>Текст сообщения*</label>
+					<textarea></textarea>
+				</div>
+				<a class="btn primary right col l2" href="#">Отправить</a>
+			</div>
+		</div>
+	</div>
+</div>
+</div> <!-- /.workarea -->
+<?php include('footer.php');?>
