311ba06ad839d92ad1429d42d96b04102f49b3e0
myaff <grabka74@gmail.com>
2015-11-18 14:00:03 +0500

    ответы форм, ошибки форм

---
+++ registration__success.php
@@ -0,0 +1,16 @@
+<?php include('header.php');?>
+<div class="workarea">
+	<div class="page-title">Регистрация завершена</div>
+	<div class="row">
+		<div class="col l8">
+			<div class="base-card">
+				<div class="card-contet no-g-padding">
+					<p>Спасибо за регистрацию на нашем сайте!</p>
+					<p>Для подтверждения регистрации, пожалуйста, пройдите по ссылке, высланной на Вашу электронную почту. Если Вы не получили письмо, проверьте его наличие в папке "Спам".</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	</br></br></br></br></br></br></br></br></br></br>
+</div> <!-- /.workarea -->
+<?php include('footer.php');?>
---
+++ vakancy__success.php
@@ -0,0 +1,15 @@
+<?php include('header.php');?>
+<div class="workarea">
+	<div class="page-title">Ваше резюме отправлено</div>
+	<div class="row">
+		<div class="col l8">
+			<div class="base-card">
+				<div class="card-contet no-g-padding">
+					<p>Спасибо! Ваше резюме отправлено в наш кадровый отдел. В случае соответствия, мы обязательно свяжемся с Вами в течение 5 рабочих дней. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	</br></br></br></br></br></br></br></br></br></br>
+</div> <!-- /.workarea -->
+<?php include('footer.php');?>
