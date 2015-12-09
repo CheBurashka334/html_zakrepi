ba2fa95061d7afbefeeeac325292c1e0bcce14b6
myaff <grabka74@gmail.com>
2015-12-09 15:24:38 +0500

    история компании правильная

--- history.php
+++ history.php
@@ -1,72 +1,184 @@
 <?php include('header.php');?>
-<div class="breadcrumbs">
-	<a class="brdcmb-link" href="#">Главная</a> / 
-	<span class="brdcmb-curpage">История компании</span>
-</div>
 <div class="workarea">
-<div class="row">
-	<div class="page-title col l10">История компании</div>
-	<div class="col l2 right-align page-title-link"><a href="#">Крепыж</a></div>
-</div>
+<div class="page-title">История компании</div>
 <div class="row history-page">
 	<div class="col l9 center">
-		<p>Компания «КрепыЖ» представлена на рынке метизной продукции и электроинструмента Тюмени с 2004 г. На сегодняшний день на территории г. Тюмени 8 филиалов в разных точках города, специально для Вашего удобства! В компании «КрепыЖ» работают только профессиональные сотрудники, которые постоянно проходят аттестации и обучения для улучшения качества обслуживания, что бы дать вам полезную консультацию! </p>
-	</div>
-	<div class="divider col l10 center"></div>
-	<div class="col l9 center">
-		<p class="color-text text-light">«Компания «КрепыЖ» представлена на рынке метизной продукции и электроинструмента Тюмени с 2004 г. На сегодняшний день на территории г. Тюмени 8 филиалов в разных точках города, специально для Вашего удобства! В компании «КрепыЖ» работают только профессиональные сотрудники, которые постоянно проходят аттестации и обучения для улучшения качества обслуживания, что бы дать вам полезную консультацию!» </p>
-	</div>
-	<div class="divider col l10 center"></div>
-	<div class="col l9 center">
-		<p>Компания «КрепыЖ» является партнером и представителем ведущих мировых и российских производителей таких, как <a href="#">BOSСH</a>, <a href="#">HITACHI</a>, <a href="#">Интерскол</a>, <a href="#">Makita</a>, <a href="#">Matrix</a>, <a href="#">Fubag</a>. На все оборудование имеются сертификаты официальных дилеров, ведь мы работаем только с прямыми поставщиками, чтобы вы покупали только качественный товар!</p>
-	</div><br/></br>
-	<div class="col l9 center">
-		<div class="shops-map small-map">
-			<div class="map" id="shops-map"></div>
-			<div class="map-overlay">
-				<p class="big-text">Магазины находятся почти во всех районах города Тюмени для вашего удобства.</p>
-				<a href="#" class="btn standart-white mediumsize center">Контакты</a>
+		<p>Компания «КрепыЖ» представлена на рынке метизной продукции и электроинструмента Тюмени с 2004 г.</p>
+		<p>На сегодняшний день на территории г. Тюмени 8 филиалов в разных точках города, специально для Вашего удобства! В компании «КрепыЖ» работают только профессиональные сотрудники, которые постоянно проходят аттестации и обучения для улучшения качества обслуживания, что бы дать вам полезную консультацию! </p>
+		<blockquote>Профессионализм сотрудников компании "Крепы<span class="primary-text">Ж</span>" и грамотный ассортимент товаров для работы и отдыха способствует созданию долгосрочных партнерских отношений с клиентами, превращая процесс продажи в удовольствие, выгодное для обеих сторон!"</blockquote>
+		<div class="subtitle">Партнёры</div>
+		<p>Компания «КрепыЖ» является партнером и представителем ведущих мировых и российских производителей. На все оборудование имеются сертификаты официальных дилеров, ведь мы работаем только с прямыми поставщиками, чтобы вы покупали только качественный товар!</p>
+		<div class="row">
+			<div class="col l8 center no-padding imgs-grid">
+				<div class="col l2"><img src="images/content/partners/bosch.png"/></div>
+				<div class="col l2 offset-l1"><img src="images/content/partners/makita.png"/></div>
+				<div class="col l2 offset-l1"><img src="images/content/partners/matrix.png"/></div>
+				<div class="col l2"><img src="images/content/partners/hitachi.png"/></div>
+				<div class="col l2 offset-l1"><img src="images/content/partners/interskol.png"/></div>
+				<div class="col l2 offset-l1"><img src="images/content/partners/condtrol.png"/></div>
+				<div class="col l2"><img src="images/content/partners/skat.png"/></div>
+				<div class="col l2 offset-l1"><img src="images/content/partners/diold.png"/></div>
+				<div class="col l2 offset-l1"><img src="images/content/partners/fubog.png"/></div>
 			</div>
 		</div>
-		<div class="brand-box small-brand-box">
-			<ul class="brand-list">
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/bosch.png" /></a> 
-				</li>
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/dewalt.png" /></a> 
-				</li>
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/makita.png" /></a> 
-				</li>
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/kovea.png" /></a> 
-				</li>
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/metabo.png" /></a> 
-				</li>
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/hitachi.png" /></a> 
-				</li>
-				<li class="brand-item">
-					<a href="#" class="brand-link img-link"><img class="brand-img" src="images/content/brands/aeg.png" /></a> 
-				</li>
-			</ul>
-		</div><br/>
 	</div>
 	<div class="col l9 center">
-		<p class="medium-text">Компанию «Крепыж» отличает:</p>
-		<ul>
-			<li>Ассортимент на самый взыскательный вкус по электроинструменту, крепежу и спец.креплениям;</li>
-			<li>4 типа цен на метизную продукцию, в зависимости от упаковки. Покупай больше - плати меньше!</li>
-			<li>Все в одном месте, товар любителей и профессионалов (от отвертки до электростанции);</li>
-			<li>В одном месте, бытовой и профессиональный электроинструмент;</li>
-			<li>Нержавеющий крепеж, чтобы уж на века!</li>
-			<li>Попробуй, прежде чем купить — тест-драйвы по технике и инструменту.</li>
-		</ul>
-		<br/>
-		<a href="catalog.php" class="btn primary bigsize center">Перейти в каталог</a>
-		<br/><br/><br/><br/><br/><br/>
+		<div class="subtitle">Доступность</div>
+		<p>Магазины находятся почти во всех районах города Тюмень для удобства!</p>
+	</div>
+	<div class="col l12 center">
+		<div class="shops-map small-map">
+			<div class="map" id="shops-map"></div>
+		</div>
+		<div class="col l8 center digit-box">
+			<div class="item">
+				<span>Более</span>
+				<div class="digit">100 000</div>
+				<div class="uppercase right-align big-text">товаров</div>
+			</div>
+			<div class="item">
+				<span>Более</span>
+				<div class="digit">250</div>
+				<span class="uppercase big-text">сотрудников</span>
+			</div>
+			<div class="item">
+				<div class="digit">12</div>
+				<span class="uppercase big-text">магазинов</span>
+			</div>
+		</div>
+	</div>
+	<div class="carousel-box">
+		<div class="carousel photo-carousel">
+			<div class="carousel-inner clearfix">
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-4.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/history/history-slide-4.png"/></div>
+			</div>
+		</div>
+		<div class="carousel-controlls">
+			<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+			<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+		</div>
+	</div>
+	<div class="col l9 center">
+		<div class="subtitle with-logo">
+			<div class="title-text">Направление Спецодежда</div>
+			<div class="title-logo"><img src="images/content/deps/specodejda.png"/></div>
+		</div>
+		<p>Так как для строительства и ремонта нужен не только инструмент, а еще и качественная рабочая одежда, то в 2007 году появилось направление "СпецОдежда".</p>
+		<p>Началось все с продажи рабочей одежды для строительных и монтажных организаций. Ассортимент постоянно обновлялся, что бы каждый смог работать в удобной одежде! Появлялась спецодежда для предприятий других сфер деятельности, а именно: медицинских учреждений, клининговых компаний, гостиниц, кафе и ресторанов, торговых организаций, охранных структур.</p>
+		<p>Магазин "Спецодежда" делает все для Вас! Широк спектр оказываемых услуг: выезд с образцами на рабочее место к корпоративным клиентам, нанесение фирменной символики, доставка товара по городу. Наличие больших складских остатков позволяет сразу же выполнить Ваши заявки.</p>
+		<p>Постоянное участие в выставках, на конференциях, спонсорская деятельность (народная ресторанная премия "Золотая вилка", спортивные мероприятия, связанные с активным отдыхом: рыбалка, стрельбища в "Стальном азарте", спортивные соревнования), благотворительность: реабилитационный центр "Соль Земли", Тюменский клуб ветеранов, Всероссийское общество слепых, тюменский центр поддержки пенсионеров-садоводов.</p>
+		<div class="row">
+			<div class="col l8 center no-padding imgs-grid">
+				<div class="col l2"><img src="images/content/partners/norfin.png"/></div>
+				<div class="col l2"><img src="images/content/partners/novatour.png"/></div>
+				<div class="col l2"><img src="images/content/partners/hsn.png"/></div>
+				<div class="col l2"><img src="images/content/partners/bshf.png"/></div>
+			</div>
+		</div>
+	</div>
+	<div class="carousel-box">
+		<div class="carousel photo-carousel">
+			<div class="carousel-inner clearfix">
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-4.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/odejda/odejda-slide-4.png"/></div>
+			</div>
+		</div>
+		<div class="carousel-controlls">
+			<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+			<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+		</div>
+	</div>
+	<div class="col l9 center">
+		<div class="subtitle with-logo">
+			<div class="title-text">Направление Сад и Огород</div>
+			<div class="title-logo"><img src="images/content/deps/sadogorod.png"/></div>
+		</div>
+		<p>В результате расширения компании КРЕПЫЖ в 2009 году так же появилось отдельное направление "Сад и Огород".</p>
+		<p>Основной принцип нашей работы — индивидуальный подход к каждому покупателю. Вы всегда получите подробную информацию по эксплуатации товара, ухода за ним, и его обслуживании. Только в наших магазинах вы получите специализированную консультацию от наших профессиональных продавцов-садоводов по семенам и удобрениям, по посадке и уходу за растениями.</p>
+		<p>Для пожилых людей 25 января 2010 года был образован Тюменский Центр Поддержки Пенсионеров-Садоводов. Направление Сад и огород также занимается и другой благотворительностью.</p>
+		<div class="row">
+			<div class="col l8 center no-padding imgs-grid">
+				<div class="col l2"><img src="images/content/partners/ballu.png"/></div>
+				<div class="col l2"><img src="images/content/partners/kronos.png"/></div>
+				<div class="col l2"><img src="images/content/partners/patriot.png"/></div>
+				<div class="col l2"><img src="images/content/partners/aelita.png"/></div>
+				<div class="col l2"><img src="images/content/partners/keter.png"/></div>
+				<div class="col l2"><img src="images/content/partners/belamos.png"/></div>
+				<div class="col l2"><img src="images/content/partners/fiskars.png"/></div>
+			</div>
+		</div>
+	</div>
+	<div class="carousel-box">
+		<div class="carousel photo-carousel">
+			<div class="carousel-inner clearfix">
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-4.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/sad/sad-slide-4.png"/></div>
+			</div>
+		</div>
+		<div class="carousel-controlls">
+			<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+			<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+		</div>
+	</div>
+	<div class="col l9 center">
+		<div class="subtitle with-logo">
+			<div class="title-text">Направление Спецтуризм</div>
+			<div class="title-logo"><img src="images/content/deps/tourism.png"/></div>
+		</div>
+		<p>Так как в нашем магазине прекрасный выбор разнообразных моделий одежды, обуви и товаров для комфортного отдыха на природе, то в результате огромного интереса к данным товарам образовалось направление "Спецтуризм".</p>
+		<p>Компания «СпецТуризм» была основана в сентябре 2009 года. В то время это был только один магазин. Сейчас это сеть из пяти магазинов, которые находятся в разных районах города, что обеспечивает проезд к ним практически из любой точки города, а удобные часы работы позволяют заглядывать к нам почаще.</p>
+		<p>Наша компания предлагает большой ассортимент продукции для туристов, как предпочитающих экономный подход к отдыху, так и для «гурманов» этого дела. Ведется также постоянное обновление и дополнение товарной линейки, исходя из Ваших пожеланий.</p>
+		<div class="row">
+			<div class="col l8 center no-padding imgs-grid">
+				<div class="col l2"><img src="images/content/partners/atemi.png"/></div>
+				<div class="col l2"><img src="images/content/partners/hideup.png"/></div>
+				<div class="col l2"><img src="images/content/partners/shimano.png"/></div>
+				<div class="col l2"><img src="images/content/partners/tohatsu.png"/></div>
+				<div class="col l2"><img src="images/content/partners/kovea.png"/></div>
+				<div class="col l2"><img src="images/content/partners/salmo.png"/></div>
+				<div class="col l2"><img src="images/content/partners/hdx.png"/></div>
+				<div class="col l2"><img src="images/content/partners/daiwa.png"/></div>
+				<div class="col l2"><img src="images/content/partners/cormoran.png"/></div>
+				<div class="col l2"><img src="images/content/partners/aiko.png"/></div>
+			</div>
+		</div>
+	</div>
+	<div class="carousel-box">
+		<div class="carousel photo-carousel">
+			<div class="carousel-inner clearfix">
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-4.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-1.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-2.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-3.png"/></div>
+				<div class="item col l3"><img src="images/content/slides/tourism/tourism-slide-4.png"/></div>
+			</div>
+		</div>
+		<div class="carousel-controlls">
+			<button class="prev"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+			<button class="next"><svg class="icon"><use xlink:href="#arrow"/></svg></button>
+		</div>
 	</div>
 </div>
 </div>
